7f308a607723f402a48776aa644a3de9cccfe6a4
Ahmedeid001 <eid0a010@gmail.com>
2024-05-04 20:09:09 +0300

    Update project files

---
+++ database/seeders/ListedSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class ListedSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = file_get_contents("C:/xamppp/htdocs/larvelnewapp/article.json");
+        $artworks = json_decode($data, true);
+
+        // Insert data into 'art' table
+        foreach ($artworks as $artwork) {
+            DB::table('art')->insert($artwork);
+        }
+    }
+}
